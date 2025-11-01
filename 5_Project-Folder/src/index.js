ff17215c3717fa4e0403abca99a8a24e0c727784
mannsk1302 <138506116+mannsk1302@users.noreply.github.com>
2025-11-01 20:14:25 +0530

    Refactor project structure and enhance server configuration.
    Moved files from `5_Project-Beginning` to `5_Project-Folder`. Added middleware (CORS, JSON parsing, CookieParser) in `app.js`, and updated `index.js` to manage server startup with proper error handling.

--- 5_Project-Folder/src/index.js
+++ 5_Project-Folder/src/index.js
@@ -1,7 +1,15 @@
 require('dotenv').config({ path: './.env' });
 const connectDB = require('./db/index.js');
+const app = require('./app.js');
 
-connectDB();
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}`));
+    })
+    .catch((error) => {
+        console.error("Error connecting to database: ", error);
+        process.exit(1);
+    });
 
 
 
