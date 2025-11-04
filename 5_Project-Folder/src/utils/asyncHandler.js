8f8466a31f0c3b76e6bd5f243e5cb7f04f268059
mannsk1302 <138506116+mannsk1302@users.noreply.github.com>
2025-11-04 16:23:44 +0530

    Add utility classes for streamlined error and response handling.
    Introduced `ApiResponse` and `ApiError` classes for standardized API responses and error management. Refactored `asyncHandler` for improved error propagation in asynchronous route handlers.

--- 5_Project-Folder/src/utils/asyncHandler.js
+++ 5_Project-Folder/src/utils/asyncHandler.js
@@ -1,3 +1,23 @@
-const asyncHandler = () => {}
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next))
+            .catch((error) => next(error));
+    }
+}
 
 module.exports = asyncHandler;
+
+
+
+
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try{
+//         await fn(req, res, next);
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message || "Server Error"
+//         });
+//     }
+// }
