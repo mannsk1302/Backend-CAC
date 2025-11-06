ead56c3f4a9674414bce4e25f9aaf6637e5d25df
mannsk1302 <138506116+mannsk1302@users.noreply.github.com>
2025-11-06 15:27:37 +0530

    Refactor `registerUser` controller for improved file handling and clean-up
    Commented out and replaced unnecessary logs. Added logic to conditionally handle `coverImage` paths. Ensured local files are removed after Cloudinary upload.

--- 5_Project-Folder/src/controllers/user.controller.js
+++ 5_Project-Folder/src/controllers/user.controller.js
@@ -5,40 +5,41 @@ const uploadOnCloudinary = require('../utils/cloudinary.js');
 const ApiResponse = require('../utils/ApiResponse.js');
 
 const registerUser = asyncHandler( async (req, res) => {
+
     // get user details from frontend
-    // validation - not empty
-    // check if user already exist?: username, email
-    // check for images, check for avatar
-    // upload them to cloudinary, avatar
-    // create user object - create entry in db
-    // remove password and refresh token field from response
-    // check for user creation
-    // return response
-
     const { fullName, email, username, password } = req.body;
-    console.log("email: ", email);
+    // console.log("email: ", email);
 
+    // validation - not empty
     if(
         [ fullName, email, username, password ].some((field) => field?.trim() === "")
     ){
         throw new ApiError(400, "User Details are required");
     }
 
-    const existedUser = User.findOne({
+    // check if user already exist?: username, email
+    const existedUser = await User.findOne({
         $or: [{ email }, { username }]
     });
 
     if(existedUser) {
         throw new ApiError(409, "User already exists");
-    };
+    }
 
+    // check for images, check for avatar
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
 
     if(!avatarLocalPath){
         throw new ApiError(403, "No avatar local path");
     }
 
+    // upload them to cloudinary, avatar
     const avatar = await uploadOnCloudinary(avatarLocalPath);
     const coverImage = await uploadOnCloudinary(coverImageLocalPath);
 
@@ -46,6 +47,7 @@ const registerUser = asyncHandler( async (req, res) => {
         throw new ApiError(403, "No avatar local path");
     }
 
+    // create user object - create entry in db
     const user = await User.create({
         fullName,
         avatar: avatar.url,
@@ -55,14 +57,17 @@ const registerUser = asyncHandler( async (req, res) => {
         password
     });
 
+    // remove password and refresh token field from response
     const createdUser = await User.findById(user.id).select(
         "-password -refreshToken"
     );
 
+    // check for user creation
     if(!createdUser){
         throw new ApiError(500, "Something went wrong while retrieving this user.");
     }
 
+    // return response
     return res.status(201).json(
         new ApiResponse({
             statusCode: 201,
