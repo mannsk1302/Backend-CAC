6fb7fc0467a9733a16ec850fb4a23c1ba9ed086c
mannsk1302 <138506116+mannsk1302@users.noreply.github.com>
2025-11-09 16:21:51 +0530

    Add playlist controller with CRUD operations
    Implemented `createPlaylist`, `getUserPlaylist`, and `getPlaylistById` controllers. Added input validation, error handling, and user-specific logic for playlists. Enhanced `getPlaylistById` with data population for related entities.

--- 5_Project-Folder/src/controllers/playlist.controller.js
+++ 5_Project-Folder/src/controllers/playlist.controller.js
@@ -1,15 +1,74 @@
 const mongoose = require('mongoose');
 const { isValidObjectId } = mongoose;
 const Playlist = require('../models/playlist.model.js');
+const Video = require('../models/video.model.js');
 const ApiResponse = require('../utils/ApiResponse.js');
 const ApiError = require('../utils/ApiError.js');
 const asyncHandler = require('../utils/asyncHandler.js');
 
-const createPlaylist = asyncHandler(async (req, res) => {});
+const createPlaylist = asyncHandler(async (req, res) => {
+    const { name, description, videos } = req.body;
+    const { userId } = req.user._id;
 
-const getUserPlaylist = asyncHandler(async (req, res) => {});
+    if(!name || name.trim() === ""){
+        throw new ApiError(400, "Playlist name is required");
+    }
 
-const getPlaylistById = asyncHandler(async (req, res) => {});
+    const playlist = await Playlist.create({
+        name,
+        description,
+        owner: userId,
+        videos: []
+    });
+
+    return res
+        .status(201)
+        .json(new ApiResponse(
+            201,
+            "Playlist created successfully",
+            { playlist }
+        ));
+});
+
+const getUserPlaylist = asyncHandler(async (req, res) => {
+    const { userId } = req.user._id;
+
+    const playlists = await Playlist.find({
+        owner: userId
+    }).sort({ createdAt: -1 });
+
+    return res
+        .status(200)
+        .json(new ApiResponse(
+            200,
+            "User playlists fetched successfully",
+            { playlists }
+        ));
+});
+
+const getPlaylistById = asyncHandler(async (req, res) => {
+    const { playlistId } = req.params;
+
+    if(!isValidObjectId(playlistId)){
+        throw new ApiError(400, "Invalid Playlist ID");
+    }
+
+    const playlist = await Playlist.findById(playlistId)
+        .populate("videos", "title thumbnail")
+        .populate("owner", "name profilePicture")
+
+    if(!playlist){
+        throw new ApiError(404, "Playlist not found");
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(
+            200,
+            "Playlist fetched successfully",
+            { playlist }
+        ));
+});
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {});
 
